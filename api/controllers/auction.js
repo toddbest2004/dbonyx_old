28015b5c2ed3c1294ef045b2e0f3dc1a6b1c139f
Todd Best <toddbest2004@gmail.com>
2016-01-06 17:43:18 -0800

    Added directive for auction results, started a new, clean stylesheet, worked more on the auction apis.

--- api/controllers/auction.js
+++ api/controllers/auction.js
@@ -5,16 +5,21 @@ var db = require("../../mongoose");
 
 router.get("/fetchauctions", function(req, res){
 	var query = req.query
-	if(!query.region||!query.realm||typeof(query.region)!=='string'||typeof(query.realm)!=='string'){
+	if(!query.realm||typeof(query.realm)!=='string'){
 		res.status(400).json({error:"Improper query string supplied."})
 		return
 	}
-	var region = query.region
-	var slugName = query.realm
-	var limit = 5
+	var realmSplit = query.realm.toLowerCase().split('-')
+	if(realmSplit.length!==2){
+		res.status(400).json({error:"Improper query string supplied."})
+		return
+	}
+	var region = realmSplit[1]
+	var slugName = realmSplit[0]
+	var limit = 25
 	var offset = 0
 
-	var query = db.auction.find({region:region,slugName:slugName,item:3184}).populate('item').skip(offset).limit(limit)
+	var query = db.auction.find({region:region,slugName:slugName,item:{$lt:1000}}).populate('item').skip(offset).limit(limit)
 	//query.populate('item', )
 	query.exec(function(err, auctions){
 		query.count(function(err, count){
--- public/app/auctions.js
+++ public/app/auctions.js
@@ -1,18 +1,26 @@
 angular.module('AuctionCtrls', [])
 .controller('AuctionCtrl', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
 	$scope.searchTerm=''
+	$scope.realminput=''
 	$scope.filters=[]
 	$scope.search=function(e){
 		e.preventDefault()
 		$http({
 			method: 'GET',
 			url: '/api/auction/fetchauctions',
-			params: {region:'us',realm:'ghostlands'}
+			params: {realm:$scope.realmInput,search:$scope.searchTerm}
 		}).then(function success(response){
 			console.log(response)
-			$scope.testauctiondata=JSON.stringify(response.data)
+			$scope.testauctions=JSON.stringify(response.data)
+			$scope.auctionResults=response.data
 		}, function error(response){
 			console.log(response)
 		})
 	}
-}])
+}]).directive('auctionResult', function(){
+	return {
+		restrict: 'E',
+		replace: true,
+		templateUrl: 'app/templates/auctionResult.html'
+	}
+})

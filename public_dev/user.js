a58d9dc5ece895587bfc8617dd5ed8769ae10e30
Todd Best <toddbest2004@gmail.com>
2016-03-27 22:11:06 -0700

    Moving logic from controllers to onyxuser service, removed some debugging console logs on server side.

--- public_dev/user.js
+++ public_dev/user.js
@@ -1,30 +1,85 @@
 angular.module('UserCtrls', [])
 .factory('onyxUser', ['$http',function($http){
 	var user = {loggedin:false}
-	$http({
-		method: 'POST',
-		url: '/api/user/getUser'
-	}).then(function success(response){
-		user.username=response.data.username
-		user.loggedin=true
-	},function error(response){
-	})
+	
+	user.checkLoggedInStatus = function(){
+		$http({
+			method: 'POST',
+			url: '/api/user/getUser'
+		}).then(function success(response){
+			user.username=response.data.username
+			user.loggedin=true
+		},function error(response){
+		})
+	}
 
-	return user
-}])
-.controller('validateCtrl', ['$http','$location','$scope', '$routeParams', function($http,$location,$scope,$routeParams){
 	//validates user's email (by clicking link created and sent to email)
-	$http({
-		method: 'POST',
-		url: '/api/user/validate',
-		data: {username:$routeParams.user,validateString:$routeParams.validateString}
-	}).then(function success(response){
-		// console.log("Success!")
-		$location.url('/')
-	},function error(response){
-		//TODO:Handle login error
-		$scope.error=response.data.error
-	})
+	user.validateUser = function(username, validateString, cb){
+		$http({
+			method: 'POST',
+			url: '/api/user/validate',
+			data: {username:username,validateString:validateString}
+		}).then(function success(response){
+			console.log("Success!")
+			return response
+		},function error(response){
+			//TODO:Handle login error
+			return cb(response.data.error)
+		})
+	}
+
+	user.login=function(email, password){
+		// console.log('a')
+		$http({
+			method: 'POST',
+			url: '/api/user/login',
+			data: {email:email,password:password}
+		}).then(function success(response){
+			// console.log($scope.user)
+			user.username=response.data.username
+			user.email=response.data.email
+			user.loggedin=true
+		},function error(response){
+			//TODO: Hanlde login error
+		})
+	}
+
+	user.logout=function(){
+		$http({
+			method: 'POST',
+			url: '/api/user/logout'
+		}).then(function success(response){
+			user.username=''
+			user.email=''
+			user.loggedin=false
+		},function error(response){
+			//TODO: handle logout error
+		})
+	}
+
+	user.register=function(username, email, password1, password2, callback){
+		$http({
+			method: 'POST',
+			url: '/api/user/register',
+			data: {username:username,email:email,password1:password1,password2:password2}
+		}).then(function success(response){
+			user.username=''
+			user.email=''
+			user.loggedin=false
+			return callback(true)
+			//TODO: Show Register Success!
+		},function error(response){
+			return callback(null, response.data.error)
+			//TODO: handle register error
+		})
+	}
+	
+	user.checkLoggedInStatus()
+	return user 
+}])
+.controller('validateCtrl', ['$http','$location','$scope', '$routeParams', 'onyxUser', function($http,$location,$scope,$routeParams, onyxUser){
+	//validates user's email (by clicking link created and sent to email)
+	onyxUser.validateUser($routeParams.user, $routeParams.validateString, function(response){$scope.error=response})
 }])
 .controller('userCtrl', ['onyxUser','$scope','$http','$location',function(onyxUser,$scope,$http,$location){
 	$scope.user=onyxUser
@@ -36,54 +91,28 @@ angular.module('UserCtrls', [])
 		$scope.showRegisterForm=!$scope.showRegisterForm
 		// console.log($scope.register)
 	}
-	$scope.login=function(){
-		// console.log('a')
-		$http({
-			method: 'POST',
-			url: '/api/user/login',
-			data: {email:$scope.login.email,password:$scope.login.password}
-		}).then(function success(response){
-			// console.log($scope.user)
-			$scope.user.username=response.data.username
-			$scope.user.email=response.data.email
-			$scope.user.loggedin=true
-		},function error(response){
-			//TODO: Hanlde login error
-		})
+	$scope.login = function(){
+		onyxUser.login($scope.login.email, $scope.login.password)
 	}
 	$scope.logout=function(){
-		$http({
-			method: 'POST',
-			url: '/api/user/logout'
-		}).then(function success(response){
-			$scope.user.username=''
-			$scope.user.email=''
-			$scope.user.loggedin=false
-			$location.url('/')
-		},function error(response){
-			//TODO: handle logout error
-		})
+		onyxUser.logout()
 	}
 	$scope.toggleUserPanel = function(){
 		$scope.showUserPanel = !$scope.showUserPanel
 	}
 }])
 .directive('userRegisterForm', [function(){
-	var controller = ['$scope','$http', '$location',function($scope,$http,$location){
+	var controller = ['$scope', 'onyxUser', function($scope,onyxUser){
 		$scope.register=function(){
-			$http({
-				method: 'POST',
-				url: '/api/user/register',
-				data: {username:$scope.username,email:$scope.email,password1:$scope.password1,password2:$scope.password2}
-			}).then(function success(response){
-				$scope.user.username=''
-				$scope.user.email=''
-				$scope.user.loggedin=false
-				$scope.showRegister()
-				//TODO: Show Register Success!
-			},function error(response){
-				console.log(response.data)
-				//TODO: handle register error
+			onyxUser.register($scope.username, $scope.email, $scope.password1, $scope.password2, function(success, err){
+				if(success){
+					//TODO: Show register success
+					$scope.showRegister()
+				}else{
+					//TODO: Show register error
+					if(err)
+						console.log(err)
+				}
 			})
 		}
 	}]

5cd0cc8049751fdf2e32bc1117bf3df6ca81fb70
JSAKALYS <TASHIKANAKOTO@GMAIL.COM>
2016-04-15 16:47:42 -0700

    sassed up

--- gulpfile.js
+++ gulpfile.js
@@ -14,9 +14,15 @@ gulp.task('default', function() {
 
 gulp.task('sass', function () {
   return gulp.src('public_dev/**/*.scss')
+  	.pipe(concat('styles.css'))
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('public/'));
 });
+
+gulp.task('copy', function(){
+	return gulp.src('public_dev/**/*.html')
+	.pipe(gulp.dest('public/'))
+})
 
 gulp.task('watch', function () {
 	watch('public_dev/**/*.js', batch(function (events, done) {
@@ -24,5 +30,8 @@ gulp.task('watch', function () {
 	}))
 	watch('public_dev/**/*.scss', batch(function (events, done) {
 		gulp.start('sass', done)
+	}))
+	watch('public_dev/**/*.html', batch(function(events, done) {
+		gulp.start('copy', done)
 	}))
 })

8393f612ee29e0af957e0845a885fd9ce0c53322
Todd Best <toddbest2004@gmail.com>
2016-04-22 21:55:23 -0700

    Possible solution to gmail authentication issues.

--- config/email.js
+++ config/email.js
@@ -1,13 +1,41 @@
 var nodemailer = require('nodemailer')
-var smtpConfig = {
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // use SSL 
+
+//http://masashi-k.blogspot.com/2013/06/sending-mail-with-gmail-using-xoauth2.html
+// var transporter = nodemailer.createTransport("SMTP", {
+//   service: "Gmail",
+//   auth: {
+//     XOAuth2: {
+// 	    user: "DBOnyx",
+// 	    clientId: "699222962515-4per0krvgqfehfm0tqitg2v8aou3e189.apps.googleusercontent.com",
+// 	    clientSecret: "EqHkIvsflsWy4LSVMQc6VKV8",
+// 	    refreshToken: '1/ozR9bkqB8E6rFCMxvBYgZXld3vEjRFxQ3tkyefuEg10'
+//     }
+//   }
+// });
+
+var xoauth2 = require('xoauth2');
+ 
+// login 
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        xoauth2: xoauth2.createXOAuth2Generator({
+		    user: process.env.EMAIL_CLIENT_USER,
+		    clientId: process.env.EMAIL_CLIENT_ID,
+		    clientSecret: process.env.EMAIL_CLIENT_SECRET,
+		    refreshToken: process.env.EMAIL_REFRESH_TOKEN
+        })
     }
-}
-var transporter = nodemailer.createTransport(smtpConfig)
+});
+
+// var testMail = {
+// 	from: 'admin@dbonyx.com',
+// 	to: process.env.EMAIL_TEST_ADDRESS,
+// 	subject: 'Welcome to DBOnyx!',
+// 	html: '<a href="http://www.dbonyx.com/validate/">Click here to validate your email</a>'
+// }
+// transporter.sendMail(testMail, function(err, response){
+// 	console.log(err, response)
+// })
 
 module.exports = transporter

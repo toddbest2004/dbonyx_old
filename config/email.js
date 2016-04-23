5c5c7fae6c937754753dfb3939c7819512010947
Todd Best <toddbest2004@gmail.com>
2016-04-22 22:28:07 -0700

    Committing WITHOUT accidentally exposing api keys

--- config/email.js
+++ config/email.js
@@ -1,18 +1,5 @@
 var nodemailer = require('nodemailer')
 
-//http://masashi-k.blogspot.com/2013/06/sending-mail-with-gmail-using-xoauth2.html
-// var transporter = nodemailer.createTransport("SMTP", {
-//   service: "Gmail",
-//   auth: {
-//     XOAuth2: {
-// 	    user: "DBOnyx",
-// 	    clientId: "699222962515-4per0krvgqfehfm0tqitg2v8aou3e189.apps.googleusercontent.com",
-// 	    clientSecret: "EqHkIvsflsWy4LSVMQc6VKV8",
-// 	    refreshToken: '1/ozR9bkqB8E6rFCMxvBYgZXld3vEjRFxQ3tkyefuEg10'
-//     }
-//   }
-// });
-
 var xoauth2 = require('xoauth2');
  
 // login 
@@ -28,14 +15,14 @@ var transporter = nodemailer.createTransport({
     }
 });
 
-// var testMail = {
-// 	from: 'admin@dbonyx.com',
-// 	to: process.env.EMAIL_TEST_ADDRESS,
-// 	subject: 'Welcome to DBOnyx!',
-// 	html: '<a href="http://www.dbonyx.com/validate/">Click here to validate your email</a>'
-// }
-// transporter.sendMail(testMail, function(err, response){
-// 	console.log(err, response)
-// })
+var testMail = {
+	from: 'admin@dbonyx.com',
+	to: process.env.EMAIL_TEST_ADDRESS,
+	subject: 'Welcome to DBOnyx!',
+	html: '<a href="http://www.dbonyx.com/validate/">Click here to validate your email</a>'
+}
+transporter.sendMail(testMail, function(err, response){
+	console.log(err, response)
+})
 
 module.exports = transporter
